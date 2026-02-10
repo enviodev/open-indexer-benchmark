c2dad3f385ac33aab033f1664246e2d01a631dd2
Dmitry Zakharov <dzakh.dev@gmail.com>
2026-02-10 13:37:43 +0400

    Move sentio benchmarks in a separate folder

--- case_1_lbtc_event_only/sqd/lib/constant.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LBTC_IMPLEMENTATION = exports.LBTC_PROXY = void 0;
-exports.LBTC_PROXY = "0x8236a87084f8B84306f72007F36F2618A5634494";
-exports.LBTC_IMPLEMENTATION = "0xa1bc65ecf8bc7b2faa22c53bcc49b0376da3845a";
-//# sourceMappingURL=constant.js.map
--- case_2_lbtc_full/sqd/lib/constant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MULTICALL_ADDRESS = exports.LBTC_IMPLEMENTATION = exports.LBTC_PROXY = void 0;
-exports.LBTC_PROXY = '0x8236a87084f8B84306f72007F36F2618A5634494';
-exports.LBTC_IMPLEMENTATION = "0xa1bc65ecf8bc7b2faa22c53bcc49b0376da3845a";
-exports.MULTICALL_ADDRESS = '0x5ba1e12693dc8f9c48aad8770482f4739beed696';
-//# sourceMappingURL=constant.js.map
